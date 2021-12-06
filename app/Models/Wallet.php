00dad85b81b10e277eed21719e99e1f75385f60e
hamd36 <mohamed1999250@gmail.com>
2021-12-06 03:04:38 +0200

    type

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -11,6 +11,6 @@ class Wallet extends Model
 
     public function pr_type()
     {
-        return $this->belongsTo(PurchaseType::class, 'id', 'p_type');
+        return $this->belongsTo(PurchaseType::class, 'p_type', 'id');
     }
 }
